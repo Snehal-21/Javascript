e660c2042b1b32f21d6c547db614b2ee28323192
Snehal Navale <navalesnehal21@gmail.com>
2023-04-02 18:48:10 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,48 @@
+// var a=12345;
+// console.log(a);
+// console.log(typeof(a));
+// var b="snehal";
+// console.log(b);
+// console.log(typeof(b));
+// var c='navale';
+// console.log(c);
+// console.log(typeof(c));
+// var d=`ABC`;
+// console.log(d);
+// console.log(typeof(d));
+// var e=true;
+// console.log(e);
+// console.log(typeof(e));
+// var f=false;
+// console.log(f);
+// console.log(typeof(f));
+// var g;
+// console.log(g);
+// console.log(typeof(g));
+
+
+// var abc=2;
+// var xyz=5
+// console.log(abc+xyz)
+// console.log(abc-xyz)
+
+// var aa="123"
+// var bb=122;
+// console.log(aa+bb)
+// console.log(aa==bb)//it checks only data not datatype
+// console.log(aa===bb)//it checks data as well as datatype
+// console.log(bb-aa)
+// console.log(aa!==bb)
+
+
+
+var a=123;
+console.log(a)
+var b='ash243@##';
+console.log(b)
+
+
+var a;
+var b=123;
+console.log(a && b);//if a is defined then print b otherwise print undefined
+console.log(a || b);//if a is defined then print a  otherwise print b---it will never show undefined
