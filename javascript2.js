06c8efdf78065757a2bb5c24be3e9e3894fa2765
Snehal Navale <navalesnehal21@gmail.com>
2023-04-05 15:17:48 +0530

    Create javascript2.js

---
+++ javascript2.js
@@ -0,0 +1,56 @@
+// if(2>1){
+//     console.log("2 is greater than 1");
+// }
+// else{
+//     console.log("2is less than 1")
+// }
+
+// var a=82;
+// if(a > 80){
+//     console.log("you are over aged.")
+// }
+// else if(a==18){
+//     console.log("you are eligible for LL.")
+// }
+// else if(80<a>18){
+//     console.log("you are eligible for DL.")
+// }
+// else{
+//     console.log("you are not eligible.")
+// }
+
+
+
+// var a="e";
+// switch(a){
+//     case "a":
+//         console.log("your are a");
+//         break;
+//     case "b":
+//         console.log("your are b");
+//         break;
+//      case "c":
+//         console.log("your are c");
+//         break;
+//     default:
+//         console.log("name is different");
+// }
+
+var userage=0;
+switch(userage=85){
+
+    case (userage > 80) :
+        console.log(" over aged");
+        break;
+
+    case (userage == 18):
+        console.log("ll");
+        break;
+    case (userage > 18):
+        console.log("dl");
+        break;
+
+    default:
+        console.log("you are not eligible......")
+    
+}
