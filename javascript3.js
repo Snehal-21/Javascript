417f86ad9d5616ff4408219addf044ef5f1fc380
Snehal Navale <navalesnehal21@gmail.com>
2023-04-07 12:31:25 +0530

    Create javascript3.js

---
+++ javascript3.js
@@ -0,0 +1,93 @@
+// var num =13;
+// if ((num%2) !== 0){
+//     console.log("odd number");
+// }
+// else{
+//     console.log("even number");
+// }
+
+
+// var num=13;
+// switch(true){
+//     case ((num %2)== 0):
+//         console.log("even");
+//         break;
+//         default:
+//             console.log("odd");
+//             break;
+// }
+
+
+// var num1=15;
+// var num2=10;
+// if(num1>num2){
+//     console.log("num1 is greater");
+// }
+// else{
+//     console.log("num2 is greater");
+// }
+
+// var num1=15;
+// var num2=30;
+// switch(true){
+//     case(num1>num2):
+//     console.log("num1 is greater");
+//     break;
+//     default:
+//         console.log("num2 is greater");
+//         break;
+// }
+
+
+
+
+// num1=15;
+// num2=50;
+// num3=40;
+// if(num1>num2 && num1>num3){
+//     console.log("num1 is greater");
+// }
+// else if(num2>num1 && num2>num3){
+//     console.log("num2 is greater");
+// }
+// else{
+//     console.log("num3 is greater");
+// }
+
+// num1=15;
+// num2=50;
+// num3=40;
+// switch(true){
+//     case (num1>num2 && num1>num3):
+//         console.log("num1 is greater");
+//         break;
+//         case (num2>num1 && num2>num3):
+//             console.log("num2 is greater");
+//             break;
+//             default:
+//                 console.log("num3 is greater");
+//                 break;
+
+
+// }
+
+
+// var a=30;
+// if(a >= 5 && a <=25){
+//     console.log("number is present in the given range");
+// }
+// else{
+//     console.log("number is not present in the given range");
+// }
+
+var a=10;
+switch(true){
+    case (a >= 5 && a <=25):
+        console.log("number is present in the given range");
+        break;
+    default:
+        console.log("number is not present in the given range");
+        break;
+
+
+}
