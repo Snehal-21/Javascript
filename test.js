577017b6c12a2668c4cc80fd138fcb267b08325d
Snehal Navale <navalesnehal21@gmail.com>
2023-07-03 16:25:20 +0530

    uodate

--- test.js
+++ test.js
@@ -1,19 +1,40 @@
 //doubt
 
-// let a=0;
-// console.log(typeof(a));
-
-// let b="0";
-// console.log(typeof(b));
-
-// console.log(a==b);
+// 
 
 
 
-function display()  
-{  
-  console.log(10+20+"30");  
-}  
-display();  
+// let message = {
+//   "p1" : "John",
+//   "p2" : "Doe",
+//   "p3" : "User"
+// }
  
+// //* Output : "JohnDoeUser"
+// let empt = ""
+// for(let key in message){
+//   empt += message[key];
+// }
+// console.log(empt);
 
+// let x = 10
+
+// {
+//   let x = 20;
+//   console.log(x);
+// }
+
+// const fruits = ['apple', 'mango', 'orange'];
+
+// const [first, second, third] = fruits;
+
+// console.log(first, second, third);
+
+
+// async function logJSONData() {
+//   const response = await fetch("http://example.com/");
+//   const jsonData = await response.json();
+//   console.log(jsonData);
+// }
+
+// logJSONData();
