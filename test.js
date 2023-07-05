eff24127bfeae1d11c6cbfe8cb3a409ae5f98e49
Snehal Navale <navalesnehal21@gmail.com>
2023-07-05 17:50:31 +0530

    test

--- test.js
+++ test.js
@@ -39,13 +39,40 @@
 
 // logJSONData();
 
-let array=[4323,3212,200,1];
+// let array=[4323,3212,200,1];
 
-// for(let i=0;i<array.length;){
-//     if(array[i]==array[i+1]){
-//         array.splice(i,1)
-//     }else{
-//         i++;
-//     }
+// // for(let i=0;i<array.length;){
+// //     if(array[i]==array[i+1]){
+// //         array.splice(i,1)
+// //     }else{
+// //         i++;
+// //     }
+// // }
+// console.log(array.sort());
+
+// function func1(){
+//   setTimeout(
+//         ()=>{
+//         console.log(x);
+//         console.log(y);
+//   },3000);
+//   var x = 2;
+//   let y = 12;
 // }
-console.log(array.sort());
+// func1();
+
+
+// function func2(){
+//   for(let i = 0; i < 3; i++){
+
+//     setTimeout(()=> console.log(i),2000);
+// }
+// }
+// func2();
+
+(function(){
+  setTimeout(()=> console.log(1),2000);
+  console.log(2);
+  setTimeout(()=> console.log(3),0);
+  console.log(4);
+})();
